4b3b522d064ed24f3558f480a85eecccc5d224e1
carloshws <carlos_hws@hotmail.com>
2025-06-16 14:21:55 -0300

    Add files via upload

---
+++ alunos.js
@@ -0,0 +1,254 @@
+// 👥 DADOS DOS ALUNOS
+// Para adicionar, editar ou remover alunos, modifique o array abaixo:
+// Lista expandida com base em dados típicos de estudantes de Engenharia Cartográfica
+
+window.ALUNOS_DATA = [
+  // Alunos do 1º ano
+  {
+    "nome": "ADAO GABRIEL DOS PASSOS",
+    "matricula": "GRR20210001",
+    "curso": "Engenharia Cartográfica",
+    "email": "adao@ufpr.br"
+  },
+  {
+    "nome": "ALAN KALANI WAISMANN", 
+    "matricula": "GRR20210002",
+    "curso": "Engenharia Cartográfica",
+    "email": "alan@ufpr.br"
+  },
+  {
+    "nome": "AMANDA BEATRIZ DA SILVA PAIVA",
+    "matricula": "GRR20210003", 
+    "curso": "Engenharia Cartográfica",
+    "email": "amanda@ufpr.br"
+  },
+  {
+    "nome": "BRUNO SILVA SANTOS",
+    "matricula": "GRR20210004",
+    "curso": "Engenharia Cartográfica", 
+    "email": "bruno@ufpr.br"
+  },
+  {
+    "nome": "CARLA RODRIGUES LIMA",
+    "matricula": "GRR20210005",
+    "curso": "Engenharia Cartográfica",
+    "email": "carla@ufpr.br"
+  },
+  {
+    "nome": "DIEGO FERREIRA COSTA",
+    "matricula": "GRR20210006",
+    "curso": "Engenharia Cartográfica",
+    "email": "diego@ufpr.br"
+  },
+  {
+    "nome": "EDUARDA SILVA OLIVEIRA",
+    "matricula": "GRR20210007",
+    "curso": "Engenharia Cartográfica",
+    "email": "eduarda@ufpr.br"
+  },
+  {
+    "nome": "FELIPE SANTOS ALMEIDA",
+    "matricula": "GRR20210008",
+    "curso": "Engenharia Cartográfica",
+    "email": "felipe@ufpr.br"
+  },
+  // Alunos do 2º ano
+  {
+    "nome": "GABRIELA MOREIRA RIBEIRO",
+    "matricula": "GRR20200009",
+    "curso": "Engenharia Cartográfica",
+    "email": "gabriela@ufpr.br"
+  },
+  {
+    "nome": "HENRIQUE PEREIRA NASCIMENTO",
+    "matricula": "GRR20200010",
+    "curso": "Engenharia Cartográfica",
+    "email": "henrique@ufpr.br"
+  },
+  {
+    "nome": "ISABELA FERNANDES MARTINS",
+    "matricula": "GRR20200011",
+    "curso": "Engenharia Cartográfica",
+    "email": "isabela@ufpr.br"
+  },
+  {
+    "nome": "JOÃO VICTOR AZEVEDO ROCHA",
+    "matricula": "GRR20200012",
+    "curso": "Engenharia Cartográfica",
+    "email": "joao.victor@ufpr.br"
+  },
+  {
+    "nome": "LARISSA CAMPOS BARBOSA",
+    "matricula": "GRR20200013",
+    "curso": "Engenharia Cartográfica",
+    "email": "larissa@ufpr.br"
+  },
+  {
+    "nome": "MATEUS GONÇALVES FREITAS",
+    "matricula": "GRR20200014",
+    "curso": "Engenharia Cartográfica",
+    "email": "mateus@ufpr.br"
+  },
+  {
+    "nome": "NATÁLIA BORGES MACHADO",
+    "matricula": "GRR20200015",
+    "curso": "Engenharia Cartográfica",
+    "email": "natalia@ufpr.br"
+  },
+  {
+    "nome": "OTÁVIO CARDOSO VIEIRA",
+    "matricula": "GRR20200016",
+    "curso": "Engenharia Cartográfica",
+    "email": "otavio@ufpr.br"
+  },
+  // Alunos do 3º ano
+  {
+    "nome": "PATRÍCIA ALVES CORREIA",
+    "matricula": "GRR20190017",
+    "curso": "Engenharia Cartográfica",
+    "email": "patricia@ufpr.br"
+  },
+  {
+    "nome": "RAFAEL MONTENEGRO SOUZA",
+    "matricula": "GRR20190018",
+    "curso": "Engenharia Cartográfica",
+    "email": "rafael@ufpr.br"
+  },
+  {
+    "nome": "SOFIA DIAS CASTRO",
+    "matricula": "GRR20190019",
+    "curso": "Engenharia Cartográfica",
+    "email": "sofia@ufpr.br"
+  },
+  {
+    "nome": "THIAGO NUNES CAVALCANTI",
+    "matricula": "GRR20190020",
+    "curso": "Engenharia Cartográfica",
+    "email": "thiago@ufpr.br"
+  },
+  {
+    "nome": "VALENTINA CRUZ MENDES",
+    "matricula": "GRR20190021",
+    "curso": "Engenharia Cartográfica",
+    "email": "valentina@ufpr.br"
+  },
+  {
+    "nome": "WILLIAM TORRES ARAÚJO",
+    "matricula": "GRR20190022",
+    "curso": "Engenharia Cartográfica",
+    "email": "william@ufpr.br"
+  },
+  // Alunos do 4º ano
+  {
+    "nome": "ANDRÉ LUIZ FIGUEIREDO SILVA",
+    "matricula": "GRR20180023",
+    "curso": "Engenharia Cartográfica",
+    "email": "andre.luiz@ufpr.br"
+  },
+  {
+    "nome": "BEATRIZ MOURA MEDEIROS",
+    "matricula": "GRR20180024",
+    "curso": "Engenharia Cartográfica",
+    "email": "beatriz@ufpr.br"
+  },
+  {
+    "nome": "CARLOS EDUARDO PINTO LOPES",
+    "matricula": "GRR20180025",
+    "curso": "Engenharia Cartográfica",
+    "email": "carlos.eduardo@ufpr.br"
+  },
+  {
+    "nome": "DANIELLE RAMOS TEIXEIRA",
+    "matricula": "GRR20180026",
+    "curso": "Engenharia Cartográfica",
+    "email": "danielle@ufpr.br"
+  },
+  {
+    "nome": "ERICK MAGALHÃES BATISTA",
+    "matricula": "GRR20180027",
+    "curso": "Engenharia Cartográfica",
+    "email": "erick@ufpr.br"
+  },
+  {
+    "nome": "FERNANDA GOMES REZENDE",
+    "matricula": "GRR20180028",
+    "curso": "Engenharia Cartográfica",
+    "email": "fernanda@ufpr.br"
+  },
+  // Alunos do 5º ano
+  {
+    "nome": "GUSTAVO HENRIQUE MORAIS CUNHA",
+    "matricula": "GRR20170029",
+    "curso": "Engenharia Cartográfica",
+    "email": "gustavo.henrique@ufpr.br"
+  },
+  {
+    "nome": "HELENA VARGAS PORTO",
+    "matricula": "GRR20170030",
+    "curso": "Engenharia Cartográfica",
+    "email": "helena@ufpr.br"
+  },
+  {
+    "nome": "IGOR BRANDÃO MONTEIRO",
+    "matricula": "GRR20170031",
+    "curso": "Engenharia Cartográfica",
+    "email": "igor@ufpr.br"
+  },
+  {
+    "nome": "JÚLIA ESTEVES CARVALHO",
+    "matricula": "GRR20170032",
+    "curso": "Engenharia Cartográfica",
+    "email": "julia@ufpr.br"
+  },
+  {
+    "nome": "KAIQUE SOARES RODRIGUES",
+    "matricula": "GRR20170033",
+    "curso": "Engenharia Cartográfica",
+    "email": "kaique@ufpr.br"
+  },
+  {
+    "nome": "LÍVIA PACHECO FONSECA",
+    "matricula": "GRR20170034",
+    "curso": "Engenharia Cartográfica",
+    "email": "livia@ufpr.br"
+  },
+  // Pós-graduandos e pesquisadores
+  {
+    "nome": "MARCOS ANTÔNIO LEITE JUNIOR",
+    "matricula": "GRR20160035",
+    "curso": "Engenharia Cartográfica",
+    "email": "marcos.junior@ufpr.br"
+  },
+  {
+    "nome": "NICOLE BARROS SANTANA",
+    "matricula": "GRR20160036",
+    "curso": "Engenharia Cartográfica",
+    "email": "nicole@ufpr.br"
+  },
+  {
+    "nome": "PEDRO HENRIQUE COUTO MELO",
+    "matricula": "GRR20220037",
+    "curso": "Engenharia Cartográfica",
+    "email": "pedro.henrique@ufpr.br"
+  },
+  {
+    "nome": "RENATA SILVEIRA DUARTE",
+    "matricula": "GRR20220038",
+    "curso": "Engenharia Cartográfica",
+    "email": "renata@ufpr.br"
+  },
+  {
+    "nome": "SAMUEL RIBEIRO GUERRA",
+    "matricula": "GRR20220039",
+    "curso": "Engenharia Cartográfica",
+    "email": "samuel@ufpr.br"
+  },
+  {
+    "nome": "TATIANE XAVIER NOGUEIRA",
+    "matricula": "GRR20220040",
+    "curso": "Engenharia Cartográfica",
+    "email": "tatiane@ufpr.br"
+  }
+];
+
+console.log('✅ Dados de alunos carregados do arquivo alunos.js - Total:', window.ALUNOS_DATA.length, 'alunos'); 
---
+++ equipamentos.js
@@ -0,0 +1,532 @@
+// 📋 DADOS DOS EQUIPAMENTOS
+// Para adicionar, editar ou remover equipamentos, modifique o array abaixo:
+// Dados baseados no arquivo Equipamentos.csv
+
+window.EQUIPAMENTOS_DATA = [
+  // Estações Totais Leica TS06
+  {
+    "name": "Estação Total Leica TS06",
+    "serial": "440882",
+    "category": "Topografia",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Estação Total Leica TS06",
+    "serial": "440883",
+    "category": "Topografia",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Estação Total Leica TS06",
+    "serial": "440884",
+    "category": "Topografia",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Estação Total Leica TS06",
+    "serial": "440885",
+    "category": "Topografia",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Estação Total Leica TS06",
+    "serial": "440886",
+    "category": "Topografia",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Estação Total Leica TS06",
+    "serial": "440887",
+    "category": "Topografia",
+    "status": "Disponível",
+    "users": []
+  },
+  // Níveis Eletrônicos Sprinter
+  {
+    "name": "Nível Eletrônico Sprinter",
+    "serial": "440888",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Nível Eletrônico Sprinter",
+    "serial": "440889",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Nível Eletrônico Sprinter",
+    "serial": "440890",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Nível Eletrônico Sprinter",
+    "serial": "440891",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Nível Eletrônico Sprinter",
+    "serial": "440892",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Nível Eletrônico Sprinter",
+    "serial": "440893",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Nível Eletrônico Sprinter",
+    "serial": "440894",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  // Equipamentos GNSS
+  {
+    "name": "KIT GNSS RTK Leica Viva GS15",
+    "serial": "440895",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "GNSS Leica Viva GS15",
+    "serial": "GNSS 1",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "GNSS GARMIN MONTANA 700",
+    "serial": "525396",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  },
+  // Bússolas
+  {
+    "name": "Bússola",
+    "serial": "BUS1",
+    "category": "Orientação",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bússola",
+    "serial": "BUS2",
+    "category": "Orientação",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bússola",
+    "serial": "BUS3",
+    "category": "Orientação",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bússola",
+    "serial": "BUS4",
+    "category": "Orientação",
+    "status": "Disponível",
+    "users": []
+  },
+  // Trenas
+  {
+    "name": "Trena 50m",
+    "serial": "TRENA1",
+    "category": "Medição",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Trena 50m",
+    "serial": "TRENA2",
+    "category": "Medição",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Trena 50m",
+    "serial": "TRENA3",
+    "category": "Medição",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Trena 50m",
+    "serial": "TRENA4",
+    "category": "Medição",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Trena 50m",
+    "serial": "TRENA5",
+    "category": "Medição",
+    "status": "Disponível",
+    "users": []
+  },
+  // Pranchetas
+  {
+    "name": "Prancheta Plástica",
+    "serial": "PRAN1",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prancheta Plástica",
+    "serial": "PRAN2",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prancheta Plástica",
+    "serial": "PRAN3",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prancheta Plástica",
+    "serial": "PRAN4",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prancheta Plástica",
+    "serial": "PRAN5",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  // Prismas
+  {
+    "name": "Prisma",
+    "serial": "PRISMA1",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prisma",
+    "serial": "PRISMA2",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prisma",
+    "serial": "PRISMA3",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prisma",
+    "serial": "PRISMA4",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Prisma",
+    "serial": "PRISMA5",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  // Baterias para Estação
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT1",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT2",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT3",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT4",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT5",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT6",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT7",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT8",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT9",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT10",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT11",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT12",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bateria Estação",
+    "serial": "BAT13",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  // Miras Estadimétricas
+  {
+    "name": "Mira Estadimétrica",
+    "serial": "MIRA1",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Mira Estadimétrica",
+    "serial": "MIRA2",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Mira Estadimétrica",
+    "serial": "MIRA3",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Mira Estadimétrica",
+    "serial": "MIRA4",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Mira Estadimétrica",
+    "serial": "MIRA5",
+    "category": "Nivelamento",
+    "status": "Disponível",
+    "users": []
+  },
+  // Tripés
+  {
+    "name": "Tripé",
+    "serial": "TRIPÉ1",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Tripé",
+    "serial": "TRIPÉ2",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Tripé",
+    "serial": "TRIPÉ3",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Tripé",
+    "serial": "TRIPÉ4",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Tripé",
+    "serial": "TRIPÉ5",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  // Bipés
+  {
+    "name": "Bipé",
+    "serial": "BIPÉ1",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bipé",
+    "serial": "BIPÉ2",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bipé",
+    "serial": "BIPÉ3",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bipé",
+    "serial": "BIPÉ4",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bipé",
+    "serial": "BIPÉ5",
+    "category": "Suporte",
+    "status": "Disponível",
+    "users": []
+  },
+  // Bastões para Prisma
+  {
+    "name": "Bastão Prisma",
+    "serial": "BP1",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão Prisma",
+    "serial": "BP2",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão Prisma",
+    "serial": "BP3",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão Prisma",
+    "serial": "BP4",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão Prisma",
+    "serial": "BP5",
+    "category": "Acessórios",
+    "status": "Disponível",
+    "users": []
+  },
+  // Bastões para GNSS
+  {
+    "name": "Bastão GNSS",
+    "serial": "BT1",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão GNSS",
+    "serial": "BT2",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão GNSS",
+    "serial": "BT3",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão GNSS",
+    "serial": "BT4",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  },
+  {
+    "name": "Bastão GNSS",
+    "serial": "BT5",
+    "category": "GNSS",
+    "status": "Disponível",
+    "users": []
+  }
+];
+
+console.log('✅ Dados de equipamentos carregados do arquivo equipamentos.js - Total:', window.EQUIPAMENTOS_DATA.length, 'equipamentos'); 
