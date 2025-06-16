6b2f4d5d955b16644a8d62ee945e7640c7f7f346
carloshws <carlos_hws@hotmail.com>
2025-06-16 16:34:12 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -1,254 +1,64 @@
-// 👥 DADOS DOS ALUNOS
-// Para adicionar, editar ou remover alunos, modifique o array abaixo:
-// Lista expandida com base em dados típicos de estudantes de Engenharia Cartográfica
-
 window.ALUNOS_DATA = [
-  // Alunos do 1º ano
-  {
-    "nome": "ADAO GABRIEL DOS PASSOS",
-    "matricula": "GRR20210001",
-    "curso": "Engenharia Cartográfica",
-    "email": "adao@ufpr.br"
-  },
-  {
-    "nome": "ALAN KALANI WAISMANN", 
-    "matricula": "GRR20210002",
-    "curso": "Engenharia Cartográfica",
-    "email": "alan@ufpr.br"
-  },
-  {
-    "nome": "AMANDA BEATRIZ DA SILVA PAIVA",
-    "matricula": "GRR20210003", 
-    "curso": "Engenharia Cartográfica",
-    "email": "amanda@ufpr.br"
-  },
-  {
-    "nome": "BRUNO SILVA SANTOS",
-    "matricula": "GRR20210004",
-    "curso": "Engenharia Cartográfica", 
-    "email": "bruno@ufpr.br"
-  },
-  {
-    "nome": "CARLA RODRIGUES LIMA",
-    "matricula": "GRR20210005",
-    "curso": "Engenharia Cartográfica",
-    "email": "carla@ufpr.br"
-  },
-  {
-    "nome": "DIEGO FERREIRA COSTA",
-    "matricula": "GRR20210006",
-    "curso": "Engenharia Cartográfica",
-    "email": "diego@ufpr.br"
-  },
-  {
-    "nome": "EDUARDA SILVA OLIVEIRA",
-    "matricula": "GRR20210007",
-    "curso": "Engenharia Cartográfica",
-    "email": "eduarda@ufpr.br"
-  },
-  {
-    "nome": "FELIPE SANTOS ALMEIDA",
-    "matricula": "GRR20210008",
-    "curso": "Engenharia Cartográfica",
-    "email": "felipe@ufpr.br"
-  },
-  // Alunos do 2º ano
-  {
-    "nome": "GABRIELA MOREIRA RIBEIRO",
-    "matricula": "GRR20200009",
-    "curso": "Engenharia Cartográfica",
-    "email": "gabriela@ufpr.br"
-  },
-  {
-    "nome": "HENRIQUE PEREIRA NASCIMENTO",
-    "matricula": "GRR20200010",
-    "curso": "Engenharia Cartográfica",
-    "email": "henrique@ufpr.br"
-  },
-  {
-    "nome": "ISABELA FERNANDES MARTINS",
-    "matricula": "GRR20200011",
-    "curso": "Engenharia Cartográfica",
-    "email": "isabela@ufpr.br"
-  },
-  {
-    "nome": "JOÃO VICTOR AZEVEDO ROCHA",
-    "matricula": "GRR20200012",
-    "curso": "Engenharia Cartográfica",
-    "email": "joao.victor@ufpr.br"
-  },
-  {
-    "nome": "LARISSA CAMPOS BARBOSA",
-    "matricula": "GRR20200013",
-    "curso": "Engenharia Cartográfica",
-    "email": "larissa@ufpr.br"
-  },
-  {
-    "nome": "MATEUS GONÇALVES FREITAS",
-    "matricula": "GRR20200014",
-    "curso": "Engenharia Cartográfica",
-    "email": "mateus@ufpr.br"
-  },
-  {
-    "nome": "NATÁLIA BORGES MACHADO",
-    "matricula": "GRR20200015",
-    "curso": "Engenharia Cartográfica",
-    "email": "natalia@ufpr.br"
-  },
-  {
-    "nome": "OTÁVIO CARDOSO VIEIRA",
-    "matricula": "GRR20200016",
-    "curso": "Engenharia Cartográfica",
-    "email": "otavio@ufpr.br"
-  },
-  // Alunos do 3º ano
-  {
-    "nome": "PATRÍCIA ALVES CORREIA",
-    "matricula": "GRR20190017",
-    "curso": "Engenharia Cartográfica",
-    "email": "patricia@ufpr.br"
-  },
-  {
-    "nome": "RAFAEL MONTENEGRO SOUZA",
-    "matricula": "GRR20190018",
-    "curso": "Engenharia Cartográfica",
-    "email": "rafael@ufpr.br"
-  },
-  {
-    "nome": "SOFIA DIAS CASTRO",
-    "matricula": "GRR20190019",
-    "curso": "Engenharia Cartográfica",
-    "email": "sofia@ufpr.br"
-  },
-  {
-    "nome": "THIAGO NUNES CAVALCANTI",
-    "matricula": "GRR20190020",
-    "curso": "Engenharia Cartográfica",
-    "email": "thiago@ufpr.br"
-  },
-  {
-    "nome": "VALENTINA CRUZ MENDES",
-    "matricula": "GRR20190021",
-    "curso": "Engenharia Cartográfica",
-    "email": "valentina@ufpr.br"
-  },
-  {
-    "nome": "WILLIAM TORRES ARAÚJO",
-    "matricula": "GRR20190022",
-    "curso": "Engenharia Cartográfica",
-    "email": "william@ufpr.br"
-  },
-  // Alunos do 4º ano
-  {
-    "nome": "ANDRÉ LUIZ FIGUEIREDO SILVA",
-    "matricula": "GRR20180023",
-    "curso": "Engenharia Cartográfica",
-    "email": "andre.luiz@ufpr.br"
-  },
-  {
-    "nome": "BEATRIZ MOURA MEDEIROS",
-    "matricula": "GRR20180024",
-    "curso": "Engenharia Cartográfica",
-    "email": "beatriz@ufpr.br"
-  },
-  {
-    "nome": "CARLOS EDUARDO PINTO LOPES",
-    "matricula": "GRR20180025",
-    "curso": "Engenharia Cartográfica",
-    "email": "carlos.eduardo@ufpr.br"
-  },
-  {
-    "nome": "DANIELLE RAMOS TEIXEIRA",
-    "matricula": "GRR20180026",
-    "curso": "Engenharia Cartográfica",
-    "email": "danielle@ufpr.br"
-  },
-  {
-    "nome": "ERICK MAGALHÃES BATISTA",
-    "matricula": "GRR20180027",
-    "curso": "Engenharia Cartográfica",
-    "email": "erick@ufpr.br"
-  },
-  {
-    "nome": "FERNANDA GOMES REZENDE",
-    "matricula": "GRR20180028",
-    "curso": "Engenharia Cartográfica",
-    "email": "fernanda@ufpr.br"
-  },
-  // Alunos do 5º ano
-  {
-    "nome": "GUSTAVO HENRIQUE MORAIS CUNHA",
-    "matricula": "GRR20170029",
-    "curso": "Engenharia Cartográfica",
-    "email": "gustavo.henrique@ufpr.br"
-  },
-  {
-    "nome": "HELENA VARGAS PORTO",
-    "matricula": "GRR20170030",
-    "curso": "Engenharia Cartográfica",
-    "email": "helena@ufpr.br"
-  },
-  {
-    "nome": "IGOR BRANDÃO MONTEIRO",
-    "matricula": "GRR20170031",
-    "curso": "Engenharia Cartográfica",
-    "email": "igor@ufpr.br"
-  },
-  {
-    "nome": "JÚLIA ESTEVES CARVALHO",
-    "matricula": "GRR20170032",
-    "curso": "Engenharia Cartográfica",
-    "email": "julia@ufpr.br"
-  },
-  {
-    "nome": "KAIQUE SOARES RODRIGUES",
-    "matricula": "GRR20170033",
-    "curso": "Engenharia Cartográfica",
-    "email": "kaique@ufpr.br"
-  },
-  {
-    "nome": "LÍVIA PACHECO FONSECA",
-    "matricula": "GRR20170034",
-    "curso": "Engenharia Cartográfica",
-    "email": "livia@ufpr.br"
-  },
-  // Pós-graduandos e pesquisadores
-  {
-    "nome": "MARCOS ANTÔNIO LEITE JUNIOR",
-    "matricula": "GRR20160035",
-    "curso": "Engenharia Cartográfica",
-    "email": "marcos.junior@ufpr.br"
-  },
-  {
-    "nome": "NICOLE BARROS SANTANA",
-    "matricula": "GRR20160036",
-    "curso": "Engenharia Cartográfica",
-    "email": "nicole@ufpr.br"
-  },
-  {
-    "nome": "PEDRO HENRIQUE COUTO MELO",
-    "matricula": "GRR20220037",
-    "curso": "Engenharia Cartográfica",
-    "email": "pedro.henrique@ufpr.br"
-  },
-  {
-    "nome": "RENATA SILVEIRA DUARTE",
-    "matricula": "GRR20220038",
-    "curso": "Engenharia Cartográfica",
-    "email": "renata@ufpr.br"
-  },
-  {
-    "nome": "SAMUEL RIBEIRO GUERRA",
-    "matricula": "GRR20220039",
-    "curso": "Engenharia Cartográfica",
-    "email": "samuel@ufpr.br"
-  },
-  {
-    "nome": "TATIANE XAVIER NOGUEIRA",
-    "matricula": "GRR20220040",
-    "curso": "Engenharia Cartográfica",
-    "email": "tatiane@ufpr.br"
-  }
+  { "nome": "﻿Nome", "matricula": "Matricula" },
+  { "nome": "ADAO GABRIEL DOS PASSOS", "matricula": "GRR20256268" },
+  { "nome": "ALAN KALANI WAISMANN", "matricula": "GRR20234068" },
+  { "nome": "AMANDA BEATRIZ DA SILVA PAIVA", "matricula": "GRR20202479" },
+  { "nome": "AMAURI FELIPE RAUBER", "matricula": "GRR20193507" },
+  { "nome": "ANA LETICIA MORAES DE LIMA", "matricula": "GRR20244788" },
+  { "nome": "ANA LUIZA DE OLIVEIRA FREITAS", "matricula": "GRR20220278" },
+  { "nome": "ANDERSON CARLOS LOPES", "matricula": "GRR20182687" },
+  { "nome": "ANDRE AUGUSTO ARAUJO BAPTISTA", "matricula": "GRR20224190" },
+  { "nome": "BEATRIZ GOMES MARCOLA", "matricula": "GRR20256326" },
+  { "nome": "BEATRIZ NOEMIA GOMES SEMCHECHEM", "matricula": "GRR20234070" },
+  { "nome": "BRUNO MARTINS SILVA", "matricula": "GRR20245729" },
+  { "nome": "CARLOS HENRIQUE NASCIMENTO FERREIRA", "matricula": "GRR20205274" },
+  { "nome": "CAROLLYNE VITORIA SANTOS DA SILVA", "matricula": "GRR20235745" },
+  { "nome": "CAUA LEANDRO FERNANDES BARBOSA", "matricula": "GRR20256159" },
+  { "nome": "CYRO ALVES DA SILVA NETO", "matricula": "GRR20202604" },
+  { "nome": "DANDARA GABRIELA VIEIRA", "matricula": "GRR20252122" },
+  { "nome": "DANIEL PRADO SANTOS", "matricula": "GRR20256405" },
+  { "nome": "DOUGLAS SANTOS DE OLIVEIRA", "matricula": "GRR20235895" },
+  { "nome": "EDUARDA DOS SANTOS LACERDA", "matricula": "GRR20193355" },
+  { "nome": "EDUARDO FERNANDO RODRIGUES DE TOLEDO", "matricula": "GRR20176795" },
+  { "nome": "EDUARDO FERREIRA SPATAS", "matricula": "GRR20251257" },
+  { "nome": "EDUARDO HENRIQUE LIMA", "matricula": "GRR20205170" },
+  { "nome": "EDUARDO POLIMANTE", "matricula": "GRR20250554" },
+  { "nome": "ELIESER LAGOS DE SOUZA", "matricula": "GRR20220273" },
+  { "nome": "EMANUELLY DE SOUZA SALLES", "matricula": "GRR20223913" },
+  { "nome": "ERICA RAFAELA BERGOSSI", "matricula": "GRR20251104" },
+  { "nome": "ESTER THASSIANA DOS SANTOS", "matricula": "GRR20244323" },
+  { "nome": "FRANCISCO DE QUEIROZ LUNA", "matricula": "GRR20235891" },
+  { "nome": "GABRIEL GOMES DA SILVA", "matricula": "GRR20206599" },
+  { "nome": "GABRIEL THOMAZ HORTENCE", "matricula": "GRR20193179" },
+  { "nome": "GABRIELLY ANDRADE PINEDA", "matricula": "GRR20241766" },
+  { "nome": "GIOVANA MONTEIRO RODRIGUES", "matricula": "GRR20243400" },
+  { "nome": "HELOISA DE FARIAS ALBERTO", "matricula": "GRR20251851" },
+  { "nome": "IGOR PONTES RIBEIRO", "matricula": "GRR20244293" },
+  { "nome": "IZABELA DE ARAUJO TEZOLIN", "matricula": "GRR20245687" },
+  { "nome": "JOAO PEDRO MACIEL PEREIRA", "matricula": "GRR20240792" },
+  { "nome": "JOAO PEDRO ROGELIN SOUZA", "matricula": "GRR20220277" },
+  { "nome": "KAUE FELLIPE LIMA DA SILVA", "matricula": "GRR20220272" },
+  { "nome": "LAIS ANASTACIO DE LIMA", "matricula": "GRR20242994" },
+  { "nome": "LARA VITORIA MIGUEL", "matricula": "GRR20242961" },
+  { "nome": "LEONARDO VINÍCIUS NEPOMUCENO ABALOS", "matricula": "GRR20193195" },
+  { "nome": "LUCAS DIAS AMORIM", "matricula": "GRR20234777" },
+  { "nome": "LUCAS LENARTOVICZ FIGUEIREDO", "matricula": "GRR20202482" },
+  { "nome": "LUIS GUSTAVO GUISSONI", "matricula": "GRR20254958" },
+  { "nome": "LUIZ GUSTAVO SOARES", "matricula": "GRR20193222" },
+  { "nome": "MARIA CLARA DE OLIVEIRA CASAGRANDE", "matricula": "GRR20244232" },
+  { "nome": "MARIA DAIANE PEREIRA DOS SANTOS", "matricula": "GRR20234776" },
+  { "nome": "MARIA EDUARDA MARQUES DA SILVA", "matricula": "GRR20244172" },
+  { "nome": "MARIA EDUARDA PASSOS CARVALHO", "matricula": "GRR20234772" },
+  { "nome": "MARIA RITA DA SILVA", "matricula": "GRR20245819" },
+  { "nome": "MATHEUS BIAZIM DONA", "matricula": "GRR20251444" },
+  { "nome": "MATHEUS DE OLIVEIRA MARTINS", "matricula": "GRR20245862" },
+  { "nome": "MATHEUS MEIRELES DOS SANTOS", "matricula": "GRR20224189" },
+  { "nome": "MATHEUS RAMIRO CARLOS", "matricula": "GRR20234065" },
+  { "nome": "MIRELLA BELINELLI MARTINS", "matricula": "GRR20255272" },
+  { "nome": "NATIELY GARCIA DE SOUZA", "matricula": "GRR20234069" },
+  { "nome": "NIKOLAS APARECIDO ALVES DA SILVA", "matricula": "GRR20250740" },
+  { "nome": "TAYLA MARTINHA MARIANO SODRE", "matricula": "GRR20255252" },
+  { "nome": "VINICIUS SILVA DE OLIVEIRA", "matricula": "GRR20235892" },
+  { "nome": "VITOR ANDRADE REGOLIN", "matricula": "GRR20224191" },
+  { "nome": "YAGO JORGE LOPES PEIXOTO", "matricula": "GRR20205127" },
 ];
-
-console.log('✅ Dados de alunos carregados do arquivo alunos.js - Total:', window.ALUNOS_DATA.length, 'alunos'); 
